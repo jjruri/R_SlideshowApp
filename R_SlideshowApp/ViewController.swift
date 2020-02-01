0af98cfa6be969f120edebcd0750e4a1d4e9c2de
jjruri <jj.ruri11280506@gmail.com>
2020-02-01 22:27:52 +0900

    Initial Commit

---
+++ R_SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  R_SlideshowApp
+//
+//  Created by 佐藤るり on 2020/02/01.
+//  Copyright © 2020 junya.satou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
