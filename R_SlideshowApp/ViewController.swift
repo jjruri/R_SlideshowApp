cbd9a914d7efb40992f132b6795b85ae9cd2f756
jjruri <jj.ruri11280506@gmail.com>
2020-02-03 16:54:11 +0900

    リスト化した画像を呼びだす処理を記載

--- R_SlideshowApp/ViewController.swift
+++ R_SlideshowApp/ViewController.swift
@@ -10,16 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var img1 = UIImage(named:"haru1") //★画像はどこに格納するのがいいの？Assertフォルダにしているページもある
-    var img2 = UIImage(named:"haru2")
-    var img3 = UIImage(named:"haru3")
-    var img4 = UIImage(named:"haru4")
+    //var img1 = UIImage(named:"haru1") //★画像はどこに格納するのがいいの？Assertフォルダにしているページもある
+    //var img2 = UIImage(named:"haru2")
+    //var img3 = UIImage(named:"haru3")
+    //var img4 = UIImage(named:"haru4")
     
     @IBOutlet weak var imageview1: UIImageView!
     @IBAction func back(_ sender: Any) {
     }
     @IBAction func play(_ sender: Any) {
-        imageview1.image = img4
     }
     @IBAction func go(_ sender: Any) {
         
@@ -36,8 +35,29 @@ class ViewController: UIViewController {
             "haru3",
             "haru4"
         ]
+        
+        // 範囲より下を指している場合、最後の画像を表示
+        if dispImageNo < 0 {
+            dispImageNo = 2
+        }
+        
+        // 範囲より上を指している場合、最初の画像を表示
+        if dispImageNo > 2 {
+            dispImageNo = 0
+        }
+        
+        // 表示している画像の番号から名前を取り出し
+        let name = imageNameArray[dispImageNo]
+        
+        // 画像を読み込み
+        let image = UIImage(named: name)
+        
+        // Image Viewに読み込んだ画像をセット
+        imageview1.image = image
     }
     
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
