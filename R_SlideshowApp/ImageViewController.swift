e08b550adabd50efc611d2d3d1fb5cf1ddb4c679
jjruri <jj.ruri11280506@gmail.com>
2020-02-10 14:06:51 +0900

    画像をタップすると次の画面へ遷移

---
+++ R_SlideshowApp/ImageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImageViewController.swift
+//  R_SlideshowApp
+//
+//  Created by 佐藤るり on 2020/02/10.
+//  Copyright © 2020 junya.satou. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- R_SlideshowApp/ViewController.swift
+++ R_SlideshowApp/ViewController.swift
@@ -122,6 +122,6 @@ class ViewController: UIViewController {
         displayImage()
         
     }
-        
+    
 }
 
