78ba369877ca882ded783b2aa3e6775e65675fd1
jjruri <jj.ruri11280506@gmail.com>
2020-02-10 14:38:31 +0900

    2画面目で画像を表示する記述を追記

--- R_SlideshowApp/ImageViewController.swift
+++ R_SlideshowApp/ImageViewController.swift
@@ -10,13 +10,37 @@ import UIKit
 
 class ImageViewController: UIViewController {
     
+    @IBOutlet weak var imageview2: UIImageView!
+    
+    
     // 受け取るためのプロパティ（変数）を宣言しておく
     var dispImageNo_big = 0
+    
+    // 表示している画像の番号を元に画像を表示する
+    func displayImage2() {
+        //画像の名前の配列
+        let imageNameArray2 = [
+            "haru4",
+            "haru3",
+            "haru2"
+        ]
+        //表示している画像の番号から名前を取り出し
+            let name2 = imageNameArray2[dispImageNo_big]
+               
+        // 画像を読み込み
+            let image2 = UIImage(named: name2)
+               
+        // Image Viewに読み込んだ画像をセット
+        imageview2.image = image2
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        imageview2.image = image2
+        
     }
     
 
